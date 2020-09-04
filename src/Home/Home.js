183c2146067a2f9202c8bca5df2ccea314fe641d
xarczuz <chjunchi.chiu@gmail.com>
2020-09-04 14:31:53 +0200

    spinning cube

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './Home.css';
 import LetterAvatars from '../Components/LetterAvatars';
+import SpinningCube from '../Components/SpinningCube';
 
 export default class Home extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Home extends Component {
         <LetterAvatars />
         <div className="lander">
           <h1>Home page</h1>
+          <SpinningCube />
         </div>
       </div>
     );
